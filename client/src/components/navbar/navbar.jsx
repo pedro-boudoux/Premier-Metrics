52b691a31aa543919c1a4dec5d247c2ea4ace630
pedro-boudoux <peboudoux@outlook.com>
2025-06-05 00:18:48 -0400

    navbar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,25 +1,25 @@
-import React from "react"
-import './navbar.css'
+import React from "react";
+import './navbar.css';
 
 export const Navbar = () => {
     return (
         <div className="navbar">
             <div className="left">
-
                 <a id="websiteLogo" href="">
                     <img src="images/logo.png" alt="Premier Metrics Logo" />
-                    <span>Premier Metrics</span>
+                    <span className="brand-name">Premier Metrics</span>
                 </a>
-                
             </div>
 
             <div className="middle">
-                this is the middle part
+                <input className="search-bar" type="text" placeholder="Look up.." />
             </div>
 
             <div className="right">
-                this is the right part
+                <a href="#">Compare</a>
+                <a href="#">Teams</a>
+                <a href="#">Rank</a>
             </div>
         </div>
-    )
-}
+    );
+};
