110b1e8a6e742c39fe0203e1ec1e3fbf98f23a5c
pedro-boudoux <peboudoux@outlook.com>
2025-07-03 22:00:12 -0400

    started working on footer

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,30 @@ function App() {
         </div>
       </div>
 
-      <LeagueTable></LeagueTable>
+      <div>
+        <LeagueTable></LeagueTable>
+      </div>
+
+      <div>
+        <h2>About Premier Metrics</h2>
+        <p>Premier Metrics is an in-depth Premier League statistics comparison website built by Pedro Boudoux, a Computer Science student at the University of Guelph.</p>
+        <p>Originally created as a fun side project to learn React and PostgreSQL, Premier Metrics evolved into a full-featured web app that allows users to explore and compare player and team stats from the 2024/25 Premier League season. The site offers an interactive and visual experience, making complex football metrics more accessible and engaging for fans, analysts, and fantasy managers alike.</p>
+
+        <h3>Data Sources & Disclaimer</h3>
+        <p>All statistical data is sourced from FBref and the official Premier League website. All rights to these statistics belong to their respective owners. This project is strictly for educational and non-commercial use.</p>
+
+        <h3>Technologies Used</h3>
+        <ul>
+          <li>Frontend: React & CSS</li>
+          <li>Backend: PostgreSQL & Express</li> 
+          <li>Scraping & Data Processing: Python & Pandas</li>
+          <li>Data Visualization: DON'T KNOW HAVEN'T DONE THAT YET!!</li>
+          <li>Other: REST APIs, Git, Figma (for design mockups!), etc.. </li>
+        </ul>
+
+        <a href="" >Contact Me!</a>
+
+      </div>
 
 
 
---
+++ client/src/components/navbar/footer.jsx
@@ -0,0 +1,14 @@
+import { React, useState, useEffect } from "react";
+
+export const Footer = () => {
+    <div id="footer-container">
+        <p>Premier Metrics</p>
+
+        <div>
+            <a>Facebook</a>
+            <a>LinkedIn</a>
+            <a>YouTube</a>
+            <a>Instagram</a>
+        </div>
+    </div>
+}
