a9de66e4a7c7eb4a3ec9ce7b6d82ee8e0fae7532
Pedro <peboudoux@outlook.com>
2026-01-21 15:41:10 -0500

    Updated search bar placeholder

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -21,13 +21,13 @@ export const Navbar = () => {
           <div className="flex flex-col relative w-full md:w-96">
             <div className="relative">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 pointer-events-none" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
               </svg>
               <input
                 className="w-full pl-9 pr-4 py-2 rounded-full border-none text-sm md:text-base text-gray-400 focus:outline-none focus:ring-2 focus:ring-premier-blue"
                 type="text"
                 value={inputValue}
-                placeholder="Search players & teams..."
+                placeholder="Search players..."
                 onChange={(e) => setInputValue(e.target.value)}
                 onFocus={() => setIsSearchFocused(true)}
                 onBlur={(e) => {
